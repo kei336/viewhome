8947540f50af243f1b5f0dbdab6912535518c22f
kei366 <kei.abc36@gmail.com>
2020-09-10 14:57:36 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+ 
   root 'home_pages#top'
   get  '/about', to:'home_pages#about'
+  get '/signup', to:'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
