21a4e5f9ad02c720947b3c92e9eefc58c497edcc
kei366 <kei.abc36@gmail.com>
2020-09-24 23:05:55 +0900

    Add ajax5

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   root   'home_pages#top'
   get    'sessions/new'
-  get    '/about',                to: 'home_pages#about'
-  get    '/signup',               to: 'users#new'
-  get    '/login',                to: 'sessions#new'
-  post   '/login',                to: 'sessions#create'
-  delete '/logout',               to: 'sessions#destroy'
-  post  'likes/:post_id/create',  to: 'likes#create'
-  post  'likes/:post_id/destroy', to: 'likes#destroy'
-  get   'users/:id/likes',        to: 'users#likes'
+  get    '/about',                 to: 'home_pages#about'
+  get    '/signup',                to: 'users#new'
+  get    '/login',                 to: 'sessions#new'
+  post   '/login',                 to: 'sessions#create'
+  delete '/logout',                to: 'sessions#destroy'
+  post    'posts/:post_id/likes',  to: 'likes#create'
+  delete  'posts/:post_id/likes',  to: 'likes#destroy'
+  get   'users/:id/likes',         to: 'users#likes'
   resources :users do
     member do
       get :following, :followers
