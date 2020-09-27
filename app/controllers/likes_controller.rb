11fee904ef200513d77c0b38d5212700df3e553a
kei366 <kei.abc36@gmail.com>
2020-09-27 09:56:04 +0900

    add like ranking

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,11 @@ class LikesController < ApplicationController
   before_action :set_post
 
   def create
-    @like = Like.create(user_id: current_user.id,post_id: @post.id)
+    @like = Like.create(user_id: current_user.id, post_id: @post.id)
   end
 
   def destroy
-    @like=Like.find_by(user_id: current_user.id, post_id: @post.id)
+    @like = Like.find_by(user_id: current_user.id, post_id: @post.id)
     @like.destroy
   end
 
